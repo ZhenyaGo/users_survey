1429fb7a151059e0e93865f87a5ce9ea736f4b6d
Zhenya <gonzay89@gmail.com>
2022-09-26 13:47:19 +0300

    add_lines_4_5

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,10 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test1");
+
+
+
+        System.out.println("Test4");
+        System.out.println("Test5");
     }
 }
