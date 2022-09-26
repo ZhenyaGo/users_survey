5e8398d5a4d883567abcaeb048aa00d4d1dfa935
Zhenya <gonzay89@gmail.com>
2022-09-26 13:50:13 +0300

    revert_merge_commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,12 +4,5 @@ public class Test {
         System.out.println("Test1");
         System.out.println("Test2");
         System.out.println("Test3");
-
-
-
-
-        System.out.println("Test4");
-        System.out.println("Test5");
-
     }
 }
